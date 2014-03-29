4e2386e62880809792c4c9ad9065f03d6edbfcfb
kibuuka <bz.denk@gmail.com>
2014-03-29 07:22:22 +0400

    cleanup. kgsl-3.10

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 6 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Wed Jun 27 00:09:15 MSK 2012"
-#define LINUX_COMPILE_TIME "00:09:15"
-#define LINUX_COMPILE_BY "den"
-#define LINUX_COMPILE_HOST "den-System-Product-Name"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
